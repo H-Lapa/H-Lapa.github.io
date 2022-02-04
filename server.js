51a90a705ee8d4c7fe9cafa878b33a697e9da0f8
Hugo Lapa <Hugolapa888@gmail.com>
2022-02-04 22:19:09 +0000

    loading webpage with ejs

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    console.log("We're at the page boss!")
+    res.render("index")
+})
 
 app.listen(3000)
