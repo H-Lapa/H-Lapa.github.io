0a2c80fa040626951535be26012ca8c89b408349
Hugo Lapa <Hugolapa888@gmail.com>
2022-02-05 21:50:58 +0000

    Linking css to ejs

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
+
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
+app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
     console.log("We're at the page boss!")
-    res.render("index")
+    res.render('index')
 })
 
 app.listen(3000)
