48c28f1d317c931bc6121c71b5f9e9bfbbcc649e
Hugo Lapa <Hugolapa888@gmail.com>
2022-02-18 20:43:23 +0000

    html form

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(express.static('public'))
+router.use('/css', express.static(__dirname + 'public/css'))
+
+
 router.get("/", (req, res) => {
     res.render('project-template')
 })
